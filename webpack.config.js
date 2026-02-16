bd05501ce5d1002627bae88a3fb413a32ead8af7
evagev2016-pixel <samyunwanarmenia@gmail.com>
2026-02-16 06:15:07 +0400

    first commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,10 @@
-const path = require('path');
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-module.exports = (env, argv) => ({
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default (env, argv) => ({
   mode: argv.mode || 'development',
   entry: './src/client/app.ts',
   output: {
